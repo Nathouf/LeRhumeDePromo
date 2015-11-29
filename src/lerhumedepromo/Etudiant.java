b40f225eceb88fe2809b49d2af40609134e0d115
didieberlin <amandine.eberlin@insa-lyon.fr>
2015-11-29 16:00:21 +0100

    Update Etudiant.java

--- src/lerhumedepromo/Etudiant.java
+++ src/lerhumedepromo/Etudiant.java
@@ -30,11 +30,12 @@ public class Etudiant {//certains attributs devront peut etre mis en public pour
            setIncubation(nbVoisins,timer);
        }
    }
-   public void setIncubation(int nbVoisins,int timer){ //methode a completer 
+  public void setIncubation(int nbVoisins,int timer){ //methode a completer 
        int C1=fragilite+sociabilite;
        int C2=0; //les constantes C2,C3,C4 vont augmenter si l'etudiant participe aux differnts evenements
        int C3=0;
        int C4=0;
+       int C5=0; //par forcement nécessaire mais plus clair = C5 correspond au nombre voisins
        
        if(this.wei && timer==2){ //si l'etudiant participe au wei et qu'on est en période de wei (semaine 2 à ajuster)
 		   C2=6; //l'etudiant a plus de rique d'imcuber la maladie
@@ -42,11 +43,28 @@ public class Etudiant {//certains attributs devront peut etre mis en public pour
 	   
 	   if(this.ppJanvier && timer==15){ //si l'etudiant participe au ppJanvier et qu'on est en periode de ppJanvier (semaine 15, à ajuster)
 		  C3=4; //l'etudiant a plus de rique d'imcuber la maladie
-	  }
-	  if(this.vingtQuatreHeures && timer==22){ //si l'etudiant participe au 24h et qu'on est en période de 24h (semaine 22 à ajuster)
+	   }
+	   if(this.vingtQuatreHeures && timer==22){ //si l'etudiant participe au 24h et qu'on est en période de 24h (semaine 22 à ajuster)
 		  C4=4; //l'etudiant a plus de rique d'imcuber la maladie
-	  }
-	  //COMPLETER METHODE avec formule finale
+	   }
+	   if( nbVoisins <= 1){
+		   //ne rien faire juste ici pour montrer que cas pris en compte
+       	   }
+           if( nbVoisins == 2 && C1 >=8){
+		   C5 = 4;
+	   }
+	   if(nbVoisins ==3 && C1>=4){
+		   C5= 4;
+	   }
+	   if(nbVoisins ==4){
+		   C5= 4;
+	   }
+	   
+	   //coefficient à modifier s'ils sont trop importants
+	   //idée formule 0,5*C1 + 0,8*C2 + 0,2*C3 + C4*0,6 + 0,5*C5
+	   if((C1 + C2 + C3 + C4 + C5) >= 20){
+		   this.incubation = true;
+	   }
 	  
    }
    public void lesSymptomesApparaissent(){//sert au premier balayage du tableau, pour que tous les incubés deviennent malades, 

c2ce7183daf51151576b836ed56b386069976f0c
mkostis <kostmarios@gmail.com>
2016-01-04 09:35:33 +0100

    comments

--- src/lerhumedepromo/EcouteurTour.java
+++ src/lerhumedepromo/EcouteurTour.java
@@ -20,9 +20,9 @@ public class EcouteurTour implements ActionListener{
 
     @Override
     	public void actionPerformed(ActionEvent e){
-       fenetreJeu.actualiserTour();
-       monPanelDessin.repaint();
-       fenetreJeu.setLabelTimer();
+       fenetreJeu.actualiserTour();//change le fenetreJeu par rapport le nouveau nombre des malades
+       monPanelDessin.repaint();// repaint pour colorier rouges les élèves qui sont devenus malades
+       fenetreJeu.setLabelTimer();//pour incrementer le nombre de semaine dans fenetreJeu
        if(fenetreJeu.monPlateau.finPartie()==1){
            fenetreJeu.ecranFinPartieLose();
        }

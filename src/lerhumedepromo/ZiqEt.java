a5b6c1340a94cf37496e64603eb32c796bb873e4
mkostis <kostmarios@gmail.com>
2016-01-04 09:13:32 +0100

    Update ZiqEt.java

--- src/lerhumedepromo/ZiqEt.java
+++ src/lerhumedepromo/ZiqEt.java
@@ -9,7 +9,7 @@ public class ZiqEt extends Etudiant{
 		weiZiqEt=false;
 	}
         
-       public void setWeiZiqEt(){//adjustement des coefficients par rapport la participation des ziqEt au Wei. La participation est initialisé au hasard avec random
+       public void setWeiZiqEt(){//adjustement des coefficients d'incubation par rapport la participation des ziqEt au Wei. La participation est initialisé au hasard avec random
             int nombreAleatoire = (int) ( Math.random() * 2 + 1);
             int timer = getTimer();
             if((timer>10)&&(timer<17)){

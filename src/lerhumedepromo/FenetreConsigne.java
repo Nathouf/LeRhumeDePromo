d78bcf6e2ad8c139e51973080162dc8c5772370c
mkostis <kostmarios@gmail.com>
2016-01-04 08:42:59 +0100

    alignement du texte consignes

--- src/lerhumedepromo/FenetreConsigne.java
+++ src/lerhumedepromo/FenetreConsigne.java
@@ -19,6 +19,8 @@ public class FenetreConsigne extends JFrame{
         cadreConsigne= new JPanel(new BorderLayout());
         //Le contenu du JLabel consignes est très long. On utilise la syntaxe HTML pour faire des retour à la ligne
         consignes = new JLabel("<html>Le but du jeu est de contaminer l'ensemble de la population étudiante avant la fin de l'année (40 semaines).<br> Au premier tour, le joueur place son virus sur l'un des étudiants afin de l'infecter.<br> Au fur et à mesure des semaines, le virus se propage.<br> Après une semaine d'incubation, les étudiants infectés tombent malade.<br> Différents évènements viennent augmenter le risque pour les étudiants de tomber malade <br> (WEI pour la semaine 1, postPartiels de janvier semaine 15, 24h de l'INSA semaine 28). <br> Les étudiants des différentes filières ont des caractéristiques différentes et ne tombent pas tous aussi facilement malade. <br> A vous de bien placer votre virus au premier tour. <br> Bonne chance! </html>");
+        consignes.setVerticalAlignment(SwingConstants.CENTER);
+        consignes.setHorizontalAlignment(JLabel.CENTER);
         fermerFenetre = new JButton("Jouer");
         this.fenJeu = fenJeu;
         

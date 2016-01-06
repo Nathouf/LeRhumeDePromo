c3c3a61e7dc10f1c639627a0d6453783f69b04f7
didieberlin <amandine.eberlin@insa-lyon.fr>
2016-01-06 21:59:08 +0100

    Update MonPanelDessin.java

--- src/lerhumedepromo/MonPanelDessin.java
+++ src/lerhumedepromo/MonPanelDessin.java
@@ -20,21 +20,21 @@ nbIconesy = fenJeu.nombreIconesy;
 	
 
 	
-	protected void paintComponent(Graphics g) {//peinture du plateau (avec les bonnes coleurs)
+	protected void paintComponent(Graphics g) {//on "peint" le tableau pour différencier les étudiants
             int largeur = this.getWidth();
             int hauteur = this.getHeight();
             super.paintComponent(g);
-
+			//on parcours le tableau et on traite les cases une à une 
             for(int lig=0 ; lig <fenJeu.monPlateau.tabEtudiant.length ; lig++) {
                 for(int col=0 ; col <fenJeu.monPlateau.tabEtudiant[0].length  ; col++) {
             
                     
-                    if(fenJeu.monPlateau.tabEtudiant[lig][col].getEtat()){//si etat true(donc si malade) il devient rouge
+                    if(fenJeu.monPlateau.tabEtudiant[lig][col].getEtat()){//si etat==true, l'étudiant est malade et il devient rouge
                         g.setColor(Color.red);     }       //sinon on regarde les autres
-                    else if(fenJeu.monPlateau.tabEtudiant[lig][col].getFiliere()==1){//d'abord on prend le filiere on fait if pour 
-                        g.setColor(Color.pink);            }//trouver les filieres et on leur assigne une couleur
-                    else if(fenJeu.monPlateau.tabEtudiant[lig][col].getFiliere()==2){//puis le fillRect dois donner la couleur qui correspond
-                        g.setColor(Color.magenta);            }//si tout se passe bien
+                    else if(fenJeu.monPlateau.tabEtudiant[lig][col].getFiliere()==1){//on trie les étudiants en fonction de leur filière 
+                        g.setColor(Color.pink);            }//puis on leur assigne une couleur
+                    else if(fenJeu.monPlateau.tabEtudiant[lig][col].getFiliere()==2){
+                        g.setColor(Color.magenta);            }
                     else if(fenJeu.monPlateau.tabEtudiant[lig][col].getFiliere()==3){
                         g.setColor(Color.yellow);            }
                     else if(fenJeu.monPlateau.tabEtudiant[lig][col].getFiliere()==4){
@@ -44,8 +44,10 @@ nbIconesy = fenJeu.nombreIconesy;
                     else if(fenJeu.monPlateau.tabEtudiant[lig][col].getFiliere()==6){
                         g.setColor(Color.cyan);            }
                     
-                  
+                  	//fillRect colorie les cases en fonction des couleurs attribuées aux filières
                		g.fillRect(col*(int)(largeur/nbIconesx),lig*(int)(hauteur/nbIconesy),(int)(largeur/nbIconesx),(int)(hauteur/nbIconesy));
+               		
+               		//pour rendre plus propre le visuel: on crée des bordures au tableau 
 			g.setColor(Color.black);
 			g.drawRect(col*(int)(largeur/nbIconesx),lig*(int)(hauteur/nbIconesy),(int)(largeur/nbIconesx),(int)(hauteur/nbIconesy));
                         }

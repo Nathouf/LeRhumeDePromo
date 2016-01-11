2e0ee2e6824daa30e5a587860f7b08b1949004d8
didieberlin <amandine.eberlin@insa-lyon.fr>
2016-01-11 08:17:08 +0100

    Update FenetreTaille.java

--- src/lerhumedepromo/FenetreTaille.java
+++ src/lerhumedepromo/FenetreTaille.java
@@ -14,23 +14,35 @@ public class FenetreTaille extends JFrame {
 
     public FenetreTaille() {
         super("Taille de plateau");
-        //Instanciations
+        //Instanciations des JTextFields
         largeur = new JTextField("", 4);
         longueur = new JTextField("", 4);
         consigne = new JLabel("Sélectionner une taille de plateau");
+        
+        //Instanciations Jlabel
         nombreEtudiantsHorizontal = new JLabel("Nombre étudiants par colonne (min 3)");
         nombreEtudiantsVertical = new JLabel("Nombre étudiants par ligne (min 3)");
         messageErreur = new JLabel();
+        
+        //Le bouton
         creerPlateau = new JButton("Créer");
+        
+        //Ecouteurs
         creerPlateau.addActionListener(new EcouteurTaille(this));
+        
+         //Instanticiations JPanel
         JPanel cadrePrincipal = new JPanel();
         JPanel tableauCommande = new JPanel();
         JPanel caseX = new JPanel(new FlowLayout());
         JPanel caseY = new JPanel(new FlowLayout());
+        
+        //Ajout des JTextField dans les JPanel
         caseX.add(nombreEtudiantsVertical);
         caseX.add(largeur);
         caseY.add(nombreEtudiantsHorizontal);
         caseY.add(longueur);
+        
+        ////Ajout des Jpanels + Ecouteurs dans les Jpanels principaux
         cadrePrincipal.setLayout(new BorderLayout());
         tableauCommande.setLayout(new BorderLayout());
         tableauCommande.add(creerPlateau, BorderLayout.SOUTH);
@@ -39,24 +51,27 @@ public class FenetreTaille extends JFrame {
         cadrePrincipal.add(consigne, BorderLayout.NORTH);
         cadrePrincipal.add(tableauCommande, BorderLayout.CENTER);
         cadrePrincipal.add(messageErreur, BorderLayout.SOUTH);
+        
+        //Caractéristiques de la fenêtre
         setContentPane(cadrePrincipal);
         setSize(600, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
-
+    
+    //Récupération des valeurs de taille de tableau mises par l'utilisateur
     public int getLongueur() {
         int x = 0;
         //verifie si input est bien un int
         try {
             x = Integer.parseInt(longueur.getText());
-        } catch (Exception e) {
-            x = -1; //symbolise l'erreur
+        } catch (Exception e) { //si ce n'est pas le cas
+            x = -1; // le -1 symbolise l'erreur
         }
         return x;
     }
-
-    public int getLargeur() {
+    
+    public int getLargeur() {//de même pour la largeur
         int y = 0;
         try {
             y = Integer.parseInt(largeur.getText());
@@ -66,7 +81,7 @@ public class FenetreTaille extends JFrame {
         return y;
     }
 
-    public void masquerFenetre() {
+    public void masquerFenetre() {//une fois qu'une fenêtre n'est plus utile
         setVisible(false);
     }
 

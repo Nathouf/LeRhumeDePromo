11c731d4460072dad0240d0e45a76024323d2cd1
didieberlin <amandine.eberlin@insa-lyon.fr>
2016-01-11 08:31:36 +0100

    Update EcouteurPremierMalade.java

--- src/lerhumedepromo/EcouteurPremierMalade.java
+++ src/lerhumedepromo/EcouteurPremierMalade.java
@@ -1,6 +1,7 @@
 
 import java.awt.event.*;
 
+//on doit expliciter toutes les méthodes de MouseListener pour que ça fonctionne, même si elles ne sont pas utiles
 public class EcouteurPremierMalade implements MouseListener {
 
     private FenetreJeu FenetreJeu;
@@ -11,27 +12,27 @@ public class EcouteurPremierMalade implements MouseListener {
         FenetreJeu = fen;
 
     }
-
+    //ne sert pas
     public void mouseExited(MouseEvent e) {
     }
-
+    //ne sert pas 
     public void mouseEntered(MouseEvent e) {
     }
-
+    //ne sert pas
     public void mouseReleased(MouseEvent e) {
     }
-
+    //ne sert pas 
     public void mousePressed(MouseEvent e) {
     }
-
-    public void mouseClicked(MouseEvent e) {//nous donne les coordonnées du clic
-        posMaladeX = e.getX();
+    //nous permet de récuperer les coordonnées du clic
+    public void mouseClicked(MouseEvent e) {
+        posMaladeX = e.getX(); //recupere les coordonnées en pixels
         posMaladeY = e.getY();
-        int x = posMaladeX / (FenetreJeu.monPanelDessin.getWidth() / FenetreJeu.nombreIconesx);
-        int y = posMaladeY / (FenetreJeu.monPanelDessin.getHeight() / FenetreJeu.nombreIconesy);
-        FenetreJeu.monPlateau.tabEtudiant[y][x].etat = true;
-        FenetreJeu.monPanelDessin.repaint();
-        FenetreJeu.DepartJeu();
+        int x = posMaladeX / (FenetreJeu.monPanelDessin.getWidth() / FenetreJeu.nombreIconesx); //on recupère la taille de chaque cases en divisant la longueur/largeur du tableau par le nombre de cases
+        int y = posMaladeY / (FenetreJeu.monPanelDessin.getHeight() / FenetreJeu.nombreIconesy); //puis on divise les coordonnées en pixel par la taille obtenue pour avoir la case ou a été faite le clic
+        FenetreJeu.monPlateau.tabEtudiant[y][x].etat = true; //on change l'état de l'étudiant placé sur la case cliquée
+        FenetreJeu.monPanelDessin.repaint(); //réinitialise le plateau
+        FenetreJeu.DepartJeu(); //lance le jeu
     }
 
 }
